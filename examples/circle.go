920f472de00450e6b86806de8f9dc6a5201481b3
Michael Fogleman <fogleman@gmail.com>
2016-02-19 13:53:33 -0500

    simplify example

--- examples/circle.go
+++ examples/circle.go
@@ -4,13 +4,9 @@ import "github.com/fogleman/gg"
 
 func main() {
 	dc := gg.NewContext(1000, 1000)
-	dc.SetRGB(1, 1, 1)
-	dc.Clear()
 	dc.DrawCircle(500, 500, 400)
-	dc.SetRGBA(0, 0, 0, 0.25)
-	dc.FillPreserve()
-	dc.SetRGB(0, 0, 0.5)
-	dc.SetLineWidth(8)
+	dc.SetRGB(0, 0, 0)
+	dc.SetLineWidth(10)
 	dc.Stroke()
 	dc.WritePNG("out.png")
 }
