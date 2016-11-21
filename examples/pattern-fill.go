8da875f641cf08c9e64019c413c3f456f51e8a33
wsw <wsw0108@gmail.com>
2016-11-21 14:57:15 +0800

    cleanup again

--- examples/pattern-fill.go
+++ examples/pattern-fill.go
@@ -1,27 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"math"
-
-	"github.com/fogleman/gg"
-)
+import "github.com/fogleman/gg"
 
 func main() {
 	im, err := gg.LoadPNG("examples/lenna.png")
 	if err != nil {
 		panic(err)
 	}
-	var x0, y0, x1, y1 float64 = 5, 5, 15, 15
-	a := math.Atan2(y1-y0, x1-x0)
-	d := a * 180 / math.Pi
-	fmt.Println(a, d)
-	a = (2*math.Pi - a)
-	d = a * 180 / math.Pi
-	fmt.Println(a, d)
-	m := gg.Translate(-x0, -y0).Rotate(a)
-	tx, ty := m.TransformPoint(5, 5)
-	fmt.Println(tx, ty)
 	pattern := gg.NewSurfacePattern(im, gg.RepeatBoth)
 	dc := gg.NewContext(600, 600)
 	dc.MoveTo(20, 20)
