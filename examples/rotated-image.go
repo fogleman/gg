5e5aa690796de05ec62982db38edc8a8f65f9d85
wsw <wsw0108@gmail.com>
2017-01-08 16:18:14 +0800

    update example 'rotated-image'

--- examples/rotated-image.go
+++ examples/rotated-image.go
@@ -4,24 +4,31 @@ import "github.com/fogleman/gg"
 
 func main() {
 	const W = 400
-	const H = 200
+	const H = 500
 	im, err := gg.LoadPNG("examples/gopher.png")
 	if err != nil {
 		panic(err)
 	}
+	iw, ih := im.Bounds().Dx(), im.Bounds().Dy()
 	dc := gg.NewContext(W, H)
 	// draw outline
 	dc.SetHexColor("#ff0000")
 	dc.SetLineWidth(1)
 	dc.DrawRectangle(0, 0, float64(W), float64(H))
 	dc.Stroke()
+	// draw full image
+	dc.SetHexColor("#0000ff")
+	dc.SetLineWidth(2)
+	dc.DrawRectangle(100, 210, float64(iw), float64(ih))
+	dc.Stroke()
+	dc.DrawImage(im, 100, 210)
 	// draw image with current matrix applied
 	dc.SetHexColor("#0000ff")
 	dc.SetLineWidth(2)
 	dc.Rotate(gg.Radians(10))
-	dc.DrawRectangle(100, 0, float64(im.Bounds().Dx()), float64(im.Bounds().Dy())/2)
+	dc.DrawRectangle(100, 0, float64(iw), float64(ih)/2+20.0)
 	dc.StrokePreserve()
 	dc.Clip()
-	dc.DrawImage(im, 100, 0)
+	dc.DrawImageAnchored(im, 100, 0, 0.0, 0.0)
 	dc.SavePNG("out.png")
 }
