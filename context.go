2da78511518a9fd86e2ed762f01814b64d92fde8
Tom Pethtel <TomPethtel@gmail.com>
2020-01-18 22:20:21 -0500

    Add method to parse font from byte array

--- context.go
+++ context.go
@@ -703,6 +703,15 @@ func (dc *Context) LoadFontFace(path string, points float64) error {
 	return err
 }
 
+func (dc *Context) LoadFontFaceFromBytes(fontData []byte, points float64) error {
+	face, err := LoadFontFaceFromBytes(fontData, points)
+	if err == nil {
+		dc.fontFace = face
+		dc.fontHeight = points * 72 / 96
+	}
+	return err
+}
+
 func (dc *Context) FontHeight() float64 {
 	return dc.fontHeight
 }
