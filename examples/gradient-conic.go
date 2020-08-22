168e105a9fd573075cfe5af5ce9b89d00b482c44
mazznoer <56464409+mazznoer@users.noreply.github.com>
2020-08-22 18:27:31 +0700

    Update gradient-conic.go

--- examples/gradient-conic.go
+++ examples/gradient-conic.go
@@ -8,22 +8,30 @@ import (
 
 func main() {
 	dc := gg.NewContext(400, 400)
-
-	grad := gg.NewConicGradient(200, 200, 90)
-	grad.AddColorStop(0.0, color.Black)
-	grad.AddColorStop(0.5, color.RGBA{255, 215, 0, 255})
-	grad.AddColorStop(1.0, color.RGBA{255, 0, 0, 255})
-
-	dc.SetRGB(1, 1, 1)
+	dc.SetRGB(0.9, 0.9, 0.9)
 	dc.Clear()
 
-	dc.SetStrokeStyle(grad)
-	dc.SetLineWidth(15)
+	grad1 := gg.NewConicGradient(200, 200, 0)
+	grad1.AddColorStop(0.0, color.Black)
+	grad1.AddColorStop(0.5, color.RGBA{255, 215, 0, 255})
+	grad1.AddColorStop(1.0, color.RGBA{255, 0, 0, 255})
+
+	grad2 := gg.NewConicGradient(200, 200, 90)
+	grad2.AddColorStop(0.00, color.RGBA{255, 0, 0, 255})
+	grad2.AddColorStop(0.16, color.RGBA{255, 255, 0, 255})
+	grad2.AddColorStop(0.33, color.RGBA{0, 255, 0, 255})
+	grad2.AddColorStop(0.50, color.RGBA{0, 255, 255, 255})
+	grad2.AddColorStop(0.66, color.RGBA{0, 0, 255, 255})
+	grad2.AddColorStop(0.83, color.RGBA{255, 0, 255, 255})
+	grad2.AddColorStop(1.00, color.RGBA{255, 0, 0, 255})
+
+	dc.SetStrokeStyle(grad1)
+	dc.SetLineWidth(20)
 	dc.DrawCircle(200, 200, 180)
 	dc.Stroke()
 
-	dc.SetFillStyle(grad)
-	dc.DrawCircle(200, 200, 155)
+	dc.SetFillStyle(grad2)
+	dc.DrawCircle(200, 200, 150)
 	dc.Fill()
 
 	dc.SavePNG("out.png")
